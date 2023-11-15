d9d06a66da073e0cb58b506e76aa9684d0b3a305
Tom Henderson <tomh@tomh.org>
2023-11-14 21:02:01 -0800

    Fix Python formatting

--- experiments/initial/plot-l4s-wifi.py
+++ experiments/initial/plot-l4s-wifi.py
@@ -10,22 +10,24 @@
 # The output file is named 'l4s-wifi.pdf'
 
 import matplotlib
-matplotlib.use("agg")
-import matplotlib.pyplot as plt
-import sys
-import numpy as np
-import re
-import os
 
-plotname = 'l4s-wifi.pdf'
+matplotlib.use("agg")
+import os
+import re
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plotname = "l4s-wifi.pdf"
 # Plot title will be any arguments passed to this script
 if len(sys.argv) > 1 and sys.argv[1] is not None:
-    title = ' '.join(sys.argv[1:])
+    title = " ".join(sys.argv[1:])
 else:
     title = "Untitled"
 space_between_subplots = 1.0
 
-f = open('wifi-dequeue-throughput.dat', 'r')
+f = open("wifi-dequeue-throughput.dat", "r")
 wifi_time = []
 wifi_samples_plot = []
 wifi_samples_avg = []
@@ -34,39 +36,39 @@ for line in f:
     t = float(columns[0])
     wifi_time.append(t)
     wifi_samples_plot.append(float(columns[1]))
-    if (t >= 2 and t <= 8):
-        wifi_samples_avg.append (float(columns[1]))
+    if t >= 2 and t <= 8:
+        wifi_samples_avg.append(float(columns[1]))
 f.close()
 
 cubic_time = []
 cubic_samples_plot = []
 cubic_samples_avg = []
-if os.path.exists('cubic-throughput.dat'):
-    f = open('cubic-throughput.dat', 'r')
+if os.path.exists("cubic-throughput.dat"):
+    f = open("cubic-throughput.dat", "r")
     for line in f:
         columns = line.split()
         t = float(columns[0])
         cubic_time.append(t)
-        cubic_samples_plot.append (float(columns[1]))
-        if (t >= 2 and t <= 8):
-            cubic_samples_avg.append (float(columns[1]))
+        cubic_samples_plot.append(float(columns[1]))
+        if t >= 2 and t <= 8:
+            cubic_samples_avg.append(float(columns[1]))
     f.close()
-    
+
 prague_time = []
 prague_samples_plot = []
 prague_samples_avg = []
-if os.path.exists('prague-throughput.dat'):
-    f = open('prague-throughput.dat', 'r')
+if os.path.exists("prague-throughput.dat"):
+    f = open("prague-throughput.dat", "r")
     for line in f:
         columns = line.split()
         t = float(columns[0])
         prague_time.append(t)
-        prague_samples_plot.append (float(columns[1]))
-        if (t >= 2 and t <= 8):
-            prague_samples_avg.append (float(columns[1]))
+        prague_samples_plot.append(float(columns[1]))
+        if t >= 2 and t <= 8:
+            prague_samples_avg.append(float(columns[1]))
     f.close()
 
-f = open('wifi-queue-bytes.dat', 'r')
+f = open("wifi-queue-bytes.dat", "r")
 bytes_time = []
 bytes_samples_plot = []
 bytes_samples_avg = []
@@ -74,12 +76,12 @@ for line in f:
     columns = line.split()
     t = float(columns[0])
     bytes_time.append(t)
-    bytes_samples_plot.append (float(columns[1]))
-    if (t >= 2 and t <= 8):
-        bytes_samples_avg.append (float(columns[1]))
+    bytes_samples_plot.append(float(columns[1]))
+    if t >= 2 and t <= 8:
+        bytes_samples_avg.append(float(columns[1]))
 f.close()
 
-f = open('wifi-dualpi2-bytes.dat', 'r')
+f = open("wifi-dualpi2-bytes.dat", "r")
 dualpi2_time = []
 dualpi2_samples_plot = []
 dualpi2_samples_avg = []
@@ -87,63 +89,89 @@ for line in f:
     columns = line.split()
     t = float(columns[0])
     dualpi2_time.append(t)
-    dualpi2_samples_plot.append (float(columns[1]))
-    if (t >= 2 and t <= 8):
-        dualpi2_samples_avg.append (float(columns[1]))
+    dualpi2_samples_plot.append(float(columns[1]))
+    if t >= 2 and t <= 8:
+        dualpi2_samples_avg.append(float(columns[1]))
 f.close()
 
 # Create subplots and orient the page in portrait mode
-fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(nrows=5, figsize=(8,11), sharex=False, sharey=False)
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(
+    nrows=5, figsize=(8, 11), sharex=False, sharey=False
+)
 fig.subplots_adjust(hspace=space_between_subplots)
-ax1.plot(wifi_time, wifi_samples_plot, marker='', color='black', label='100ms throughput samples')
-ax1.set_xlim(0,)
-ax1.set_ylim(0,120)
-ax1.set_xlabel('Time (s)')
-ax1.set_ylabel('Throughput (Mbps)')
-wifi_avg = round(sum(wifi_samples_avg)/len(wifi_samples_avg),1)
-wifi_label = r'Avg {} Mbps (2s < t < 8s)'.format(wifi_avg)
-ax1.axhline(y = wifi_avg, color = 'r', linestyle = 'dashed', label=wifi_label)
+ax1.plot(
+    wifi_time,
+    wifi_samples_plot,
+    marker="",
+    color="black",
+    label="100ms throughput samples",
+)
+ax1.set_xlim(
+    0,
+)
+ax1.set_ylim(0, 120)
+ax1.set_xlabel("Time (s)")
+ax1.set_ylabel("Throughput (Mbps)")
+wifi_avg = round(sum(wifi_samples_avg) / len(wifi_samples_avg), 1)
+wifi_label = r"Avg {} Mbps (2s < t < 8s)".format(wifi_avg)
+ax1.axhline(y=wifi_avg, color="r", linestyle="dashed", label=wifi_label)
 ax1.set_title("Wifi throughput")
-ax1.legend(loc='upper right', framealpha=1, prop={"size" : 6})
+ax1.legend(loc="upper right", framealpha=1, prop={"size": 6})
 
-ax2.plot(cubic_time, cubic_samples_plot, marker='', color='black', label='100ms throughput samples')
-ax2.set_xlim(0,)
-ax2.set_ylim(0,120)
-ax2.set_xlabel('Time (s)')
-ax2.set_ylabel('Throughput (Mbps)')
+ax2.plot(
+    cubic_time,
+    cubic_samples_plot,
+    marker="",
+    color="black",
+    label="100ms throughput samples",
+)
+ax2.set_xlim(
+    0,
+)
+ax2.set_ylim(0, 120)
+ax2.set_xlabel("Time (s)")
+ax2.set_ylabel("Throughput (Mbps)")
 if len(cubic_samples_avg):
-    cubic_avg = round(sum(cubic_samples_avg)/len(cubic_samples_avg),1)
-    cubic_label = r'Avg {} Mbps (2s < t < 8s)'.format(cubic_avg)
-    ax2.axhline(y = cubic_avg, color = 'r', linestyle = 'dashed', label=cubic_label)
+    cubic_avg = round(sum(cubic_samples_avg) / len(cubic_samples_avg), 1)
+    cubic_label = r"Avg {} Mbps (2s < t < 8s)".format(cubic_avg)
+    ax2.axhline(y=cubic_avg, color="r", linestyle="dashed", label=cubic_label)
 ax2.set_title("Cubic throughput")
-ax2.legend(loc='upper right', framealpha=1, prop={"size" : 6})
+ax2.legend(loc="upper right", framealpha=1, prop={"size": 6})
 
-ax3.plot(prague_time, prague_samples_plot, marker='', color='black', label='100ms throughput samples')
-ax3.set_xlim(0,)
-ax3.set_ylim(0,120)
-ax3.set_xlabel('Time (s)')
-ax3.set_ylabel('Throughput (Mbps)')
+ax3.plot(
+    prague_time,
+    prague_samples_plot,
+    marker="",
+    color="black",
+    label="100ms throughput samples",
+)
+ax3.set_xlim(
+    0,
+)
+ax3.set_ylim(0, 120)
+ax3.set_xlabel("Time (s)")
+ax3.set_ylabel("Throughput (Mbps)")
 if len(prague_samples_avg):
-    prague_avg = round(sum(prague_samples_avg)/len(prague_samples_avg),1)
-    prague_label = r'Avg {} Mbps (2s < t < 8s)'.format(prague_avg)
-    ax3.axhline(y = prague_avg, color = 'r', linestyle = 'dashed', label=prague_label)
+    prague_avg = round(sum(prague_samples_avg) / len(prague_samples_avg), 1)
+    prague_label = r"Avg {} Mbps (2s < t < 8s)".format(prague_avg)
+    ax3.axhline(y=prague_avg, color="r", linestyle="dashed", label=prague_label)
 ax3.set_title("Prague throughput")
-ax3.legend(loc='upper right', prop={"size" : 6})
+ax3.legend(loc="upper right", prop={"size": 6})
 
-ax4.plot(bytes_time, bytes_samples_plot, marker='', color='black', label='Bytes')
-ax4.set_ylim(0,600000)
-ax4.set_xlabel('Time (s)')
-ax4.set_ylabel('Bytes in AC_BE')
+ax4.plot(bytes_time, bytes_samples_plot, marker="", color="black", label="Bytes")
+ax4.set_ylim(0, 600000)
+ax4.set_xlabel("Time (s)")
+ax4.set_ylabel("Bytes in AC_BE")
 ax4.set_title("Bytes in Wi-Fi device AC_BE queue")
-ax4.legend(loc='upper right', prop={"size" : 6})
+ax4.legend(loc="upper right", prop={"size": 6})
 
-ax5.plot(dualpi2_time, dualpi2_samples_plot, marker='', color='black', label='Bytes')
-ax5.set_ylim(0,600000)
-ax5.set_xlabel('Time (s)')
-ax5.set_ylabel('Bytes in DualPI2')
+ax5.plot(dualpi2_time, dualpi2_samples_plot, marker="", color="black", label="Bytes")
+ax5.set_ylim(0, 600000)
+ax5.set_xlabel("Time (s)")
+ax5.set_ylabel("Bytes in DualPI2")
 ax5.set_title("Bytes in overlying DualPI2 AC_BE queue")
-ax5.legend(loc='upper right', prop={"size": 6})
+ax5.legend(loc="upper right", prop={"size": 6})
 
 fig.suptitle(title)
-plt.savefig(plotname, format='pdf')
+plt.savefig(plotname, format="pdf")
 plt.close()
--- experiments/initial/run-l4s-wifi.py
+++ experiments/initial/run-l4s-wifi.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 import os
-import sys
-import subprocess
 import shutil
+import subprocess
+import sys
 from datetime import datetime
 
 # If not executing this in an experiments subdirectory, need to change this
 # The assumed starting directory is 'experiments/<name>'
-path_to_ns3_dir =  '../../'
+path_to_ns3_dir = "../../"
 # The assumed execution directory is 'experiments/<name>/results-YYmmdd--HHMMSS'
-path_to_ns3_script = '../../../ns3'
+path_to_ns3_script = "../../../ns3"
 
 # In future, add parametric job control here (multiprocessing.Pool)
 numCubic = 1
 numPrague = 1
-numBackground = 0 
+numBackground = 0
 numBytes = 50000000
 duration = 0
 mcs = 2
@@ -23,31 +23,31 @@ limit = 65535
 scale = 1
 showProgress = 0
 
-arguments = ' --numCubic=' + str(numCubic)
-arguments += ' --numPrague=' + str(numPrague)
-arguments += ' --numBackground=' + str(numBackground)
-arguments += ' --numBytes=' + str(numBytes)
-arguments += ' --duration=' + str(duration)
-arguments += ' --mcs=' + str(mcs)
-arguments += ' --flowControl=' + str(flowControl)
-arguments += ' --limit=' + str(limit)
-arguments += ' --scale=' + str(scale)
-arguments += ' --showProgress=' + str(showProgress)
+arguments = " --numCubic=" + str(numCubic)
+arguments += " --numPrague=" + str(numPrague)
+arguments += " --numBackground=" + str(numBackground)
+arguments += " --numBytes=" + str(numBytes)
+arguments += " --duration=" + str(duration)
+arguments += " --mcs=" + str(mcs)
+arguments += " --flowControl=" + str(flowControl)
+arguments += " --limit=" + str(limit)
+arguments += " --scale=" + str(scale)
+arguments += " --showProgress=" + str(showProgress)
 
 # Build a plot title; customize as needed
-plotTitle = 'Cubic=' + str(numCubic)
-plotTitle += ' Prague=' + str(numPrague)
-plotTitle += ' Background=' + str(numBackground)
+plotTitle = "Cubic=" + str(numCubic)
+plotTitle += " Prague=" + str(numPrague)
+plotTitle += " Background=" + str(numBackground)
 
 # Make a unique timestamped results directory
 formatted_date = datetime.now().strftime("%Y%m%d-%H%M%S")
-resultsdir = 'results-' + formatted_date
+resultsdir = "results-" + formatted_date
 os.makedirs(resultsdir, exist_ok=False)
 
 # Copy the plotting file into the results directory
-shutil.copy('plot-l4s-wifi.py', resultsdir)
+shutil.copy("plot-l4s-wifi.py", resultsdir)
 # Copy the scenario program into the results directory
-shutil.copy(path_to_ns3_dir + 'scratch/l4s-wifi.cc', resultsdir)
+shutil.copy(path_to_ns3_dir + "scratch/l4s-wifi.cc", resultsdir)
 # Copy any other files here
 
 # Cd to the results directory
@@ -55,49 +55,66 @@ os.chdir(resultsdir)
 
 # Copy this script into the results directory
 script_filename = os.path.basename(__file__)
-with open(__file__, 'r') as source:
+with open(__file__, "r") as source:
     script_content = source.read()
-with open(os.path.join(os.getcwd(), script_filename), 'w') as destination_file:
+with open(os.path.join(os.getcwd(), script_filename), "w") as destination_file:
     destination_file.write(script_content)
 
 try:
-    with open('build.out', 'w') as out:
-        subprocess.run([path_to_ns3_script, 'build'], stdout=out, stderr=out, check=True)
+    with open("build.out", "w") as out:
+        subprocess.run(
+            [path_to_ns3_script, "build"], stdout=out, stderr=out, check=True
+        )
 except subprocess.CalledProcessError as e:
     print(f"Build error: {e}:  Check build.out file for error.")
     sys.exit(e.returncode)
 
 # Run l4s-wifi
-with open('run.out', 'w') as out:
-    result = subprocess.run([path_to_ns3_script, 'run', '--no-build', '--cwd', os.getcwd(), 'l4s-wifi', '--'] + arguments.split(), stdout=out, text=True)
+with open("run.out", "w") as out:
+    result = subprocess.run(
+        [
+            path_to_ns3_script,
+            "run",
+            "--no-build",
+            "--cwd",
+            os.getcwd(),
+            "l4s-wifi",
+            "--",
+        ]
+        + arguments.split(),
+        stdout=out,
+        text=True,
+    )
 
 # Save the parameters used in a 'commandlog.out' file
-with open('commandlog.out', 'w') as out:
-    out.write('ns3 run l4s-wifi' + ' -- ' + arguments + '\n')
+with open("commandlog.out", "w") as out:
+    out.write("ns3 run l4s-wifi" + " -- " + arguments + "\n")
     out.close()
 
 # Unused for now-- for future reference
 command_get_repo_time = "git rev-parse --abbrev-ref HEAD"
 command_get_repo_commit = "git rev-parse --short HEAD"
 
-branch_output = subprocess.check_output(['git', 'branch', '-vvv'])
-with open('version.out', 'w') as out:
-    command_output = branch_output.decode('utf-8').strip()
-    command_output.split('\n')[0:1]
-    out.write("# Branch name                    commit hash                   commit message\n")
-    out.write("".join(command_output.split('\n')[0:1]))
+branch_output = subprocess.check_output(["git", "branch", "-vvv"])
+with open("version.out", "w") as out:
+    command_output = branch_output.decode("utf-8").strip()
+    command_output.split("\n")[0:1]
+    out.write(
+        "# Branch name                    commit hash                   commit message\n"
+    )
+    out.write("".join(command_output.split("\n")[0:1]))
     out.close()
 
-diff_output = subprocess.check_output(['git', 'diff'])
-with open('version.diff', 'w') as out:
-    command_output = diff_output.decode('utf-8').strip()
+diff_output = subprocess.check_output(["git", "diff"])
+with open("version.diff", "w") as out:
+    command_output = diff_output.decode("utf-8").strip()
     out.write(command_output)
-    out.write('\n')
+    out.write("\n")
     out.close()
 
-# Make a plot 
-
-# Make a plot 
-subprocess.run(['python3', 'plot-l4s-wifi.py', plotTitle], stdout=subprocess.PIPE, text=True)
+# Make a plot
+subprocess.run(
+    ["python3", "plot-l4s-wifi.py", plotTitle], stdout=subprocess.PIPE, text=True
+)
 
 sys.exit()
