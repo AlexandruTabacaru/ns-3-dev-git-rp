e2e443318c627eb8dc139326d516693647aa5ac7
Shafi Khan <s.khan@cablelabs.com>
2024-03-22 22:02:22 -0600

    Import process_summary_csv function

--- experiments/multiprocess/run-l4s-wifi.py
+++ experiments/multiprocess/run-l4s-wifi.py
@@ -6,7 +6,7 @@ import sys
 import pandas as pd
 from datetime import datetime
 import multiprocessing
-from processor.processor import processResults, merge_input_with_results
+from processor.processor import processResults, merge_input_with_results, process_summary_csv
 
 def buildPlotTitle(numCubic, numPrague, numBackground):
     # Build a plot title; customize as needed
